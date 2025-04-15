a1df255aa42fe7353f63a584a8c8c865c0a6af82
Michael Ouma <Oumamichael108@gmail.com>
2025-04-15 22:20:35 +0300

    lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
-// create your App component here
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+
+  useEffect(() => {
+    // Fetch a random dog image
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDogImage(data.message);
+      });
+  }, []); // Empty dependency array = run once when component mounts
+
+  return (
+    <div className="App">
+      {dogImage ? (
+        <img src={dogImage} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
+
+export default App;
